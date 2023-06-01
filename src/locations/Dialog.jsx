bb3b77c6237669d98806ba4ea2b956534a757a5c
Clara Song <clara@contenda.co>
2023-06-01 02:02:44 -0400

    enable images toggle

--- src/locations/Dialog.jsx
+++ src/locations/Dialog.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Spinner, Stack, EntityList, Flex, Text } from '@contentful/f36-components';
+import { Spinner, Stack, EntityList, Flex, Text, Switch } from '@contentful/f36-components';
 import { useCMA, useSDK, useAutoResizer } from '@contentful/react-apps-toolkit';
 
 const BASE_URL = "https://prod.contenda.io"
@@ -7,12 +7,26 @@ const BASE_URL = "https://prod.contenda.io"
 const Dialog = () => {
   const sdk = useSDK();
   const cma = useCMA();
+
   useAutoResizer();
   const [token, setToken] = useState()
   const [contendaBlogs, setContendaBlogs] = useState([])
   const [isUploadingImages, setIsUploadingImages] = useState(false)
+  const [allowAutoImportBlogImages, setAllowAutoImportBlogImages] = useState()
 
   const { email, apiKey } = sdk.parameters.installation;
+
+
+  const saveAllowAutoImportBlogImages = async (value) => {
+    cma.editorInterface.get({ contentTypeId: sdk.ids.contentType }).then(
+      (editor) => {
+        let fieldControl = editor.controls.find(control => control.fieldId == sdk.ids.field)
+        fieldControl.settings.allowAutoImportBlogImages = value
+        setAllowAutoImportBlogImages(value)
+        return cma.editorInterface.update({ contentTypeId: sdk.ids.contentType }, editor)
+      }
+    )
+  }
 
   const fetchAllBlogsData = async () => {
     const getTokenUrl = `${BASE_URL}/api/v2/identity/token`
@@ -39,58 +53,116 @@ const Dialog = () => {
     setContendaBlogs(allBlogs);
   }
 
-  const fetchBlog = async (blogId) => {
+  const handleBlogSelection = async (blogId) => {
     console.log("token: ", token);
     const getBlogUrl = `${BASE_URL}/api/v2/content/blog/${blogId}`
 
     const blogResponse = await fetch(`${getBlogUrl}?token=${token}`)
-    const blog = await blogResponse.json();
+    let blog = await blogResponse.json();
     console.log(blogId, "blog: ", blog);
 
     blog.id = blogId
 
-    setIsUploadingImages(true)
-    let addImagePromises = []
-    let image_idx = 1
-    for (let segment of blog.segments) {
-      if (segment.segment_type === "image" || segment.segment_type === "user_image") {
-        addImagePromises.push(addImageAsset("Contenda Blog Image " + image_idx, segment).then((asset) => console.log("fetchBlog", asset)))
-        image_idx++
+    console.log("allowAutoImportBlogImages: ", allowAutoImportBlogImages);
+
+    if (allowAutoImportBlogImages) {
+      // const contentTypeId = sdk.ids.contentType
+      // let contentTypeData = await cma.contentType.get({ "contentTypeId": sdk.ids.contentType })
+      // let fieldData = contentTypeData.fields.find(field => field.id == sdk.ids.field)
+      // let enabledTypesValidationRule = fieldData.validations.find(validation => "enabledNodeTypes" in validation)
+  
+      // if (enabledTypesValidationRule && !enabledTypesValidationRule.enabledNodeTypes.includes("embedded-asset-block")) {
+      //   await sdk.dialogs.openConfirm({
+      //     title: "Your field settings don't allow for embedded blog images",
+      //     message: `Would you like to change your settings to allow \"Embedded Assets\" in this field across this content type ${contentTypeId}?`,
+      //     intent: 'positive',
+      //     confirmLabel: 'Yes, change my settings',
+      //     cancelLabel: 'No, go back to selection',
+      //   })
+      //   .then((allowChangeSettings) => {
+      //     if (allowChangeSettings) {
+      //       enableEmbeddedAssets()
+      //     }
+      //     return allowChangeSettings
+      //   })
+      // }
+
+      setIsUploadingImages(true)
+      let addImagePromises = []
+      let image_idx = 1
+      for (let segment of blog.segments) {
+        if (segment.segment_type === "image" || segment.segment_type === "user_image") {
+          addImagePromises.push(addImageAsset("Contenda Blog Image " + image_idx, segment)
+            .then((asset) => console.log("image asset", asset)))
+          image_idx++
+        }
       }
-    }
-    Promise.all(addImagePromises).then(() => {
+      Promise.all(addImagePromises)
+        .then(() => {
+          setIsUploadingImages(false)
+          console.log("images finished uploading and processing in promise.all");
+          sdk.close(blog)
+        })
+        .catch((err) => console.error(err))
+    } else {
       setIsUploadingImages(false)
+      const segmentsNoImages = blog.segments.filter(
+        segment => segment.segment_type !== "image" && segment.segment_type !== "user_image"
+      )
+      blog.segments = segmentsNoImages
       sdk.close(blog)
-    })
+    }
   }
 
   const addImageAsset = async (assetTitle, imageSegment) => {
     console.log("image id: ", imageSegment.id);
+    let assetFields = {}
+    const locale = await sdk.locales.default
+    assetFields.title = {}
+    assetFields.title[locale] = assetTitle
+    assetFields.file = {}
+    assetFields.file[locale] = {
+      contentType: 'image/jpeg',
+      fileName: imageSegment.id + '.jpeg',
+      upload: imageSegment.image_url,
+    }
     const asset = await cma.asset.createWithId(
       { assetId: imageSegment.id }, {
-      fields: {
-        title: {
-          'en-US': assetTitle
-        },
-        file: {
-          'en-US': {
-            contentType: 'image/jpeg',
-            fileName: imageSegment.id + '.jpeg',
-            upload: imageSegment.image_url,
-          }
-        }
-      }
+      fields: assetFields
     })
       .then((asset) => cma.asset.processForAllLocales({}, asset))
       // .then((asset) => cma.asset.publish({ assetId: asset.sys.id }, asset))
-      .catch((err) => console.log(err))
+      .catch((err) => console.error(err))
 
     return asset
+  }
+
+  const enableEmbeddedAssets = async () => {
+    const contentTypeId = sdk.ids.contentType
+    let contentTypeData = await cma.contentType.get({ "contentTypeId": contentTypeId })
+    let fieldData = contentTypeData.fields.find(field => field.id == sdk.ids.field)
+    let enabledTypesValidationRule = fieldData.validations.find(validation => "enabledNodeTypes" in validation)
+
+    enabledTypesValidationRule.enabledNodeTypes.push("embedded-asset-block")
+    enabledTypesValidationRule.message = "Only heading 1, heading 2, heading 3, heading 4, heading 5, heading 6, ordered list, unordered list, horizontal rule, quote, block entry, table, link to Url, link to entry, inline entry, and asset nodes are allowed"
+    cma.contentType.update({ contentTypeId: contentTypeId }, contentTypeData)
+      .then(updatedContentType => cma.contentType.publish({ contentTypeId: contentTypeId }, updatedContentType))
+      .catch(err => console.error(err))
   }
 
   useEffect(() => {
     fetchAllBlogsData()
   }, [sdk.parameters.invocation])
+
+  useEffect(() => {
+    cma.editorInterface.get({ contentTypeId: sdk.ids.contentType }).then(
+      (editor) => {
+        const allow = editor.controls.find(control => control.fieldId == sdk.ids.field).settings?.allowAutoImportBlogImages
+        setAllowAutoImportBlogImages(allow)
+      })
+  }, [sdk.editor])
+
+  useEffect(() => {console.log("useEffect sdk.contentType", sdk.contentType);}, [sdk.contentType])  
 
   if (!contendaBlogs) {
     return (
@@ -109,7 +181,18 @@ const Dialog = () => {
   }
 
   return (
-    <Stack fullWidth>
+    <Stack fullWidth flexDirection="column" spacing="none">
+      <Flex margin="spacingS" alignSelf="flex-start">
+        <Switch
+          name="allow-image-autoimport"
+          id="allow-image-autoimport"
+          isChecked={allowAutoImportBlogImages}
+          onChange={() => saveAllowAutoImportBlogImages(!allowAutoImportBlogImages)}
+          helpText="If on, will upload and process all the blog images as assets"
+        >
+          Embed Blog Images
+        </Switch>
+      </Flex>
       <EntityList style={{
         width: '100%'
       }}>
@@ -119,8 +202,8 @@ const Dialog = () => {
               key={blog.id}
               title={blog.title}
               entityType={"Page"}
-              description={`Created: ${blog.created_at?.substring(0,10)}`}
-              onClick={() => fetchBlog(blog.id)}
+              description={`Created: ${blog.created_at?.substring(0, 10)}`}
+              onClick={() => handleBlogSelection(blog.id)}
             />)
           })
         }
--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { RichTextEditor } from '@contentful/field-editor-rich-text';
-import { Form, FormControl, Button, Text, Note } from '@contentful/f36-components';
+import { Form, FormControl, Button, Text, Note, ModalLauncher } from '@contentful/f36-components';
 import { useSDK, useAutoResizer, useCMA } from '@contentful/react-apps-toolkit';
 
 
@@ -10,7 +10,6 @@ const Field = () => {
 
   const [mediaName, setMediaName] = useState()
   const [adviceErrorNotes, setAdviceErrorNotes] = useState([])  // custom notes when specific validation errors are present
-  const [didUpdateAssetEnable, setDidUpdateAssetEnable] = useState(false) // validation that's updated automatically
 
   useAutoResizer()
 
@@ -104,42 +103,22 @@ const Field = () => {
     setMediaName(blog.segments[0]?.title)
   }
 
-  const enableAssetValidation = async () => {
-    // console.log("enableAssetValidation");
-    sdk.contentType.enabledNodeTypes("embedded-asset-block")
-    const contentTypeId = sdk.contentType.sys.id
-    let contentTypeData = await cma.contentType.get({ "contentTypeId": contentTypeId })
-    let fieldData = contentTypeData.fields.find(field => field.id == sdk.field.id)
-    let enabledTypesValidationRule = fieldData.validations.find(validation => "enabledNodeTypes" in validation)
-
-    if (enabledTypesValidationRule && !enabledTypesValidationRule.enabledNodeTypes.includes("embedded-asset-block")) {
-      // console.log("need to edit validation");
-      
-      enabledTypesValidationRule.enabledNodeTypes.push("embedded-asset-block")
-      enabledTypesValidationRule.message = "Only heading 1, heading 2, heading 3, heading 4, heading 5, heading 6, ordered list, unordered list, horizontal rule, quote, block entry, table, link to Url, link to entry, inline entry, and asset nodes are allowed"
-      cma.contentType.update({ contentTypeId: contentTypeId }, contentTypeData)
-        .then(updatedContentType => cma.contentType.publish({ contentTypeId: contentTypeId }, updatedContentType))
-        .then(publishedContentType => sdk.contentType = publishedContentType)
-
-      return true
-    }
-    return false
-  }
-
   useEffect(() => {
     sdk.field.onSchemaErrorsChanged((errors) => {
       let messagesToRender = []
       for (const error of errors) {
         if (error.name === "enabledNodeTypes" && !error.message.includes("block asset")) {
-          messagesToRender.push(<Note variant="neutral">Check field settings and enable "Embedded Assets"</Note>)
+          sdk.notifier.error('Check field settings and enable "Embedded Assets"');
+          messagesToRender.push(<Note variant="negative">Check field settings and enable "Embedded Assets"</Note>)
         } else if (error.name === "size") {
-          messagesToRender.push(<Note variant="neutral">Check field settings and increase the limit number of Embedded Assets</Note>)
+          sdk.notifier.warning("Remove images or increase the Embedded Assets limit in the field settings");
+          messagesToRender.push(<Note variant="warning">Remove images or increase the Embedded Assets limit in the field settings</Note>)
         }
       }
       setAdviceErrorNotes(messagesToRender)
     })
-  }, [sdk.field.validations])
-  
+  }, [sdk.field])
+
   return (
     <>
       <Form onSubmit={() => openDialog()}>
@@ -147,7 +126,12 @@ const Field = () => {
           <Button type='submit' variant='primary'>Choose a Contenda Blog</Button>
         </FormControl>
       </Form>
-      {mediaName !== undefined ? <Text>Media title: {mediaName}</Text> : undefined}
+      {mediaName !== undefined ?
+        <>
+          <Text fontSize="fontSizeL">Video title: </Text>
+          <Text fontSize="fontSizeL" as="i" fontWeight="fontWeightMedium">{mediaName}</Text>
+        </> : undefined
+      }
       {/* {didUpdateAssetEnable ? <Note variant="neutral">Changed field's validation rule to enable "Embedded Assets" to include blog images</Note> : undefined} */}
       <RichTextEditor sdk={sdk} isInitiallyDisabled={true} />
       {adviceErrorNotes}
